02c2a396eac882b852ca7d01917f3c61b9dc7638
Hritwik Sharma <kaliprasadsharma9800@gmail.com>
2026-06-09 23:59:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,9 @@ if not st.user.is_logged_in:
     # Import and render your customized login screen layout
     from views.login import render_login
     render_login()
+    
+    # FORCE STREAMLIT TO HALT HERE UNTIL THEY CLICK LOG IN
+    st.stop() 
 else:
     # Google account authenticated successfully! Forward to your dashboard
     st.session_state["authenticated"] = True
